2485c01894d9ab0b754bb9398c960c3554811a3a
TheOneTrueNoodle <72693746+TheOneTrueNoodle@users.noreply.github.com>
2022-12-31 00:03:18 +0000

    Basic locomotive player movement

---
+++ CreatureCollectorGame/Assets/Scripts/PlayerLocomotion.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace player
+{
+    public class PlayerLocomotion : MonoBehaviour
+    {
+        private Transform cameraObject;
+        private InputHandler inputHandler;
+        private Vector3 moveDirection;
+
+        [HideInInspector] public Transform myTransform;
+        [HideInInspector] public AnimatorHandler animatorHandler;
+
+        public new Rigidbody rigidbody;
+        public GameObject normalCamera;
+
+        [Header("Stats")]
+        [SerializeField] private float movementSpeed = 5;
+        [SerializeField] private float rotationSpeed = 10;
+
+        private void Start()
+        {
+            rigidbody = GetComponent<Rigidbody>();
+            inputHandler = GetComponent<InputHandler>();
+            animatorHandler = GetComponentInChildren<AnimatorHandler>();
+            cameraObject = Camera.main.transform;
+            myTransform = transform;
+            animatorHandler.Initialize();
+        }
+
+        public void Update()
+        {
+            float delta = Time.deltaTime;
+
+            inputHandler.TickInput(delta);
+
+            moveDirection = cameraObject.forward * inputHandler.vertical;
+            moveDirection += cameraObject.right * inputHandler.horizontal;
+            moveDirection.Normalize();
+
+            float speed = movementSpeed;
+            moveDirection *= speed;
+
+            Vector3 projectedVelocity = Vector3.ProjectOnPlane(moveDirection, normalVector);
+            rigidbody.velocity = projectedVelocity;
+
+            animatorHandler.UpdateAnimatorValues(inputHandler.moveAmount, 0);
+
+            if (animatorHandler.canRotate)
+            {
+                HandleRotation(delta);
+            }
+        }
+
+        #region Movement
+
+        private Vector3 normalVector;
+        private Vector3 targetPosition;
+        private void HandleRotation(float delta)
+        {
+            Vector3 targetDirection = Vector3.zero;
+            float moveOverride = inputHandler.moveAmount;
+
+            targetDirection = cameraObject.forward * inputHandler.vertical;
+            targetDirection += cameraObject.right * inputHandler.horizontal;
+
+            targetDirection.Normalize();
+            targetDirection.y = 0;
+
+            if(targetDirection == Vector3.zero)
+            {
+                targetDirection = myTransform.forward;
+            }
+
+            float rs = rotationSpeed;
+
+            Quaternion tr = Quaternion.LookRotation(targetDirection);
+            Quaternion targetRotation = Quaternion.Slerp(myTransform.rotation, tr, rs * delta);
+
+            myTransform.rotation = targetRotation;
+        }
+
+        #endregion
+    }
+}
+
